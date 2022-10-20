fcc3c34ab29fdf60e8019b48600bb3735eca0b48
Germiprogramer <llorentemunozgerman@gmail.com>
2022-10-20 18:37:19 +0100

    Update coctel.py

--- ordenacion/coctel.py
+++ ordenacion/coctel.py
@@ -1,7 +1,15 @@
+l = [2,1,4,3]
+
+def comp_der(a,b):
+    return a == b
+    
+
+comp_der(l[1],l[2])
+
+print(l)
 
 
 def coctel(lista):
-
     izquierda = 0
     derecha = len(lista)-1
     control = True
@@ -18,3 +26,4 @@ def coctel(lista):
                 control = True
                 lista[j], lista[j-1] = lista[j-1], lista[j]
         izquierda +=1
+
